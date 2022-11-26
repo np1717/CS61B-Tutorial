d9677dfbf4576b9be5fef79c3bfd88728b45f763
np1717 <1004186200@qq.com>
2022-11-26 14:19:41 +0800

    modified some styles

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,5 +1,7 @@
 /** Performs some basic linked list tests. */
-public class LinkedListDequeTest { /* Utility method for printing out empty checks. */
+public class LinkedListDequeTest {
+
+	/* Utility method for printing out empty checks. */
 	public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
 			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
@@ -17,7 +19,7 @@ public class LinkedListDequeTest { /* Utility method for printing out empty chec
 		return true;
 	}
 
-	/* Prints a nice message based on whether a test passed. 
+	/* Prints a nice message based on whether a test passed.
 	 * The \n means newline. */
 	public static void printTestStatus(boolean passed) {
 		if (passed) {
@@ -27,20 +29,18 @@ public class LinkedListDequeTest { /* Utility method for printing out empty chec
 		}
 	}
 
-	/** Adds a few things to the list, checking isEmpty() and size() are correct, 
-	  * finally printing the results. 
-	  *
-	  * && is the "and" operation. */
+	/** Adds a few things to the list, checking isEmpty() and size() are correct,
+	 * finally printing the results.
+	 *
+	 * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines " +
-				"below (and delete this print statement).");
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst("front");
-		
+
 		// The && operator is the same as "and" in Python.
 		// It's a binary operator that returns true if both arguments true, and false otherwise.
 		passed = checkSize(1, lld1.size()) && passed;
@@ -56,7 +56,6 @@ public class LinkedListDequeTest { /* Utility method for printing out empty chec
 		lld1.printDeque();
 
 		printTestStatus(passed);
-
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
@@ -64,23 +63,19 @@ public class LinkedListDequeTest { /* Utility method for printing out empty chec
 
 		System.out.println("Running add/remove test.");
 
-		System.out.println("Make sure to uncomment the lines " +
-				"below (and delete this print statement).");
-
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		// should be empty 
+		// should be empty
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst(10);
-		// should not be empty 
+		// should not be empty
 		passed = checkEmpty(false, lld1.isEmpty()) && passed;
 
 		lld1.removeFirst();
-		// should be empty 
+		// should be empty
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
-
 	}
 
 	public static void main(String[] args) {
@@ -89,3 +84,4 @@ public class LinkedListDequeTest { /* Utility method for printing out empty chec
 		addRemoveTest();
 	}
 }
+
