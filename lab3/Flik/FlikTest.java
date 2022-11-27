71ddf6fcae1ce05837df801e230c9b898c5f4046
np1717 <1004186200@qq.com>
2022-11-27 16:05:21 +0800

    lab3 commit

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void testIsSameNumber(){
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
+
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -66,8 +66,17 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
+    @Test(timeout = 1000)
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3);
+        IntList B = IntList.reverse(A);
+        assertEquals(IntList.of(3, 2, 1), B);
+        assertNotEquals(IntList.of(1, 2, 3), A);
+        IntList C = IntList.reverse(null);
+        assertEquals(null, C);
+    }
+
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
-
 }
